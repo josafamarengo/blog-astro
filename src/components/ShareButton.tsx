950e710a660e8a3a482ce3111fe28abb951cdad3
Josafá Marengo <josafabmarengo@gmail.com>
2023-10-19 15:45:34 -0400

    feat(share-button): improve style

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -18,14 +18,17 @@ function ShareButton({ title, description, url }: ShareButtonProps){
     const [showShareItems, setShowShareItems] = useState(false);
   return (
     <button title={"Compartilhe"} class="relative">
-      <ShareIcon onClick={() => setShowShareItems(!showShareItems)} />
+      <ShareIcon
+        onClick={() => setShowShareItems(!showShareItems)}
+        style={{ fontSize: "1.25rem" }}
+      />
       {showShareItems && (
-        <div class="flex flex-col space-y-4 items-start absolute bottom-10 right-0 lg:-right-16 z-30 w-auto h-auto  bg-zinc-300 dark:bg-zinc-700 rounded-lg p-4">
+        <div class="flex flex-col space-y-4 items-start absolute bottom-11 right-0 lg:-right-16 z-30 w-auto h-auto bg-gradient-to-t from-zinc-950 from-10% to-zinc-800 rounded-lg p-4">
           <a
             href={`https://twitter.com/intent/tweet?text=${title}&url=${url}`}
             target="_blank"
             rel="noopener noreferrer"
-            class="no-underline flex items-center space-x-2 text-sm"
+            class="no-underline flex items-center space-x-2 text-sm text-white"
           >
             <span>
               <MdiTwitter />
@@ -36,7 +39,7 @@ function ShareButton({ title, description, url }: ShareButtonProps){
             href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
             target="_blank"
             rel="noopener noreferrer"
-            class="no-underline flex items-center space-x-2 text-sm"
+            class="no-underline flex items-center space-x-2 text-sm text-white"
           >
             <span>
               <MdiFacebook />
@@ -47,7 +50,7 @@ function ShareButton({ title, description, url }: ShareButtonProps){
             href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}&title=${title}&summary=${description}`}
             target="_blank"
             rel="noopener noreferrer"
-            class="no-underline flex items-center space-x-2 text-sm"
+            class="no-underline flex items-center space-x-2 text-sm text-white"
           >
             <MdiLinkedin />
             <span>LinkedIn</span>
@@ -56,7 +59,7 @@ function ShareButton({ title, description, url }: ShareButtonProps){
             href={`https://api.whatsapp.com/send?text=${title} ${url}`}
             target="_blank"
             rel="noopener noreferrer"
-            class="no-underline flex items-center space-x-2 text-sm"
+            class="no-underline flex items-center space-x-2 text-sm text-white"
           >
             <MdiWhatsapp />
             <span>WhatsApp</span>
@@ -65,7 +68,7 @@ function ShareButton({ title, description, url }: ShareButtonProps){
             href={`https://www.reddit.com/submit?url=${url}&title=${title}`}
             target="_blank"
             rel="noopener noreferrer"
-            class="no-underline flex items-center space-x-2 text-sm"
+            class="no-underline flex items-center space-x-2 text-sm text-white"
           >
             <MdiReddit />
             <span>Reddit</span>
